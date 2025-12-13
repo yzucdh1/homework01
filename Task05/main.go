25a8cd3d82330562fd2186622bf8a13439e63592
chendonghui <yzucdh@163.com>
2025-12-13 15:28:01 +0800

    加一

---
+++ Task05/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 加一
+func plusOne(digits []int) []int {
+	n := len(digits)
+
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] != 9 {
+			digits[i]++
+			for j := i + 1; j < n; j++ {
+				digits[j] = 0
+			}
+			return digits
+		}
+	}
+
+	digits = make([]int, n+1)
+	digits[0] = 1
+
+	return digits
+}
+
+func main() {
+	digits := []int{0}
+	fmt.Println(plusOne(digits))
+}
